4acfcbdc8a9bd352270caeb09442e4d49d85585e
EC2 Default User <ec2-user@ip-172-31-27-23.us-east-2.compute.internal>
2020-04-28 20:50:47 +0000

    first commit

---
+++ resources/views/instruments/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+    <main>
+        <h1>Ongaq JS Sample</h1>
+        <ul>
+            <li><a id="button" class="button" href="javascript:;"></a></li>
+        </ul>
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'InstrumentsController@index');
+
+Route::resource('instruments', 'InstrumentsController');
