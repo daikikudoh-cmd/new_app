ba782733412698ce3d4bfa109939d7779e1245ac
daikikudoh-cmd <daikikudoh313@gmail.com>
2020-04-29 01:19:53 +0000

    add js

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
-<<<<<<< HEAD
-=======
         \URL::forceScheme('https');
->>>>>>> 4acfcbdc8a9bd352270caeb09442e4d49d85585e
     }
 
     /**
